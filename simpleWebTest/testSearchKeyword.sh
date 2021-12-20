32955578487db2f665ea8ef1b9d50095375256df
k-eeer <37653119+k-eeer@users.noreply.github.com>
2021-12-20 17:20:22 +0800

    Update testSearchKeyword.sh

--- simpleWebTest/testSearchKeyword.sh
+++ simpleWebTest/testSearchKeyword.sh
@@ -9,10 +9,13 @@ echo "The functional test is beginning."
 echo "The result of functional test is:"
 pytest functionalTest.py --no-header --no-summary
 
+
+
 #Load test the performance under some simulated user searching the keyword with duckduckgo.com
 #Increase soft limit of the open file number for locust
 ulimit -S -n 999999
 gnome-terminal -- bash -c "locust -f locustFile.py -u $totalUser -r $increment --csv output/loadTest;exec bash &"& 2>/dev/null
+
 
 rm -f -- buttonClick.py
 cat>>buttonClick.py<<EOF
@@ -47,6 +50,8 @@ gnome-terminal -- bash -c "python3 buttonClick.py $totalTime;exec bash &"& 2>/de
 
 echo "The charts and table of load test result saved in ./output"
 loadCsvArr=$(sed -i 's/ /\_/g' output/loadTest_stats.csv|awk -F, 'FNR<=2{printf "%-18s\t%-20s\t%-12s\n" ,$10,$5,$17 }' output/loadTest_stats.csv)
+
+
 
 #generate load test conclusion
 rm -f -- loadTestConclusion.py
