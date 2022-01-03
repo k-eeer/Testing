b98a568ebeb6385e47423563bae08cf75e182efa
k-eeer <37653119+k-eeer@users.noreply.github.com>
2022-01-03 19:48:58 +0800

    Add files via upload

---
+++ simpleRestApiTest/README.md
@@ -0,0 +1,27 @@
+# testCalculator-description and usage:
+
+The script will generates a simple REST API by Flask and test it. Including 1) a functional test by Pytest and requests 2)a Performance test(Load test) by Locust. 
+
+
+
+    
+    $sh apiTest.sh
+    #input the number of total users and increment per seconds for load test.
+
+
+# environment and prerequisites:
+
+  * Ubuntu 20.04 
+  * Flask 1.1.2
+  * requests 2.22.0
+  * pytest 5.4.3
+  * Locust 1.1.1
+
+
+
+# demo and ouput:
+![image](https://github.com/k-eeer/theTests/blob/main/simpleRestApiTest/output/demoRestApiTest.png)
+![image](https://github.com/k-eeer/theTests/blob/main/simpleRestApiTest/output/responseTimes(ms).png)
+
+
+As the output of demo, two tests were passed.
---
+++ simpleRestApiTest/buttonClick.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import sys,time
+
+filePath='loadTest.png'
+totalTime=sys.argv[1]
+driver=webdriver.Firefox()
+driver.get("http://127.0.0.1:8089/")
+time.sleep(5)
+try:
+	startButton=driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/form/button")
+	startButton.click()
+except:
+	pass
+time.sleep(int(totalTime)+20)
+#loadTestTable=driver.find_element_by_tag_name("table")
+#loadTestTable.screenshot("output/loadTestTable.png")
+chartButton=driver.find_element_by_xpath("/html/body/div[2]/div[3]/nav/ul/li[2]/a")
+time.sleep(2)
+chartButton.click()
+time.sleep(int(totalTime)+20)
+canvasChart=driver.find_elements_by_tag_name("canvas")
+fileName=["totalRequestsPerSecond","responseTimes(ms)","numberOfUsers"]
+for i in range(len(fileName)):
+	name=fileName[i]
+	canvasChart[i].screenshot(f"output/{name}.png")
