52e5c616abdbddc3271d057c3af40d56765211f9
k-eeer <37653119+k-eeer@users.noreply.github.com>
2022-06-14 16:15:07 +0800

    Update loadTestConclusion.py

--- simpleWebTest/loadTestConclusion.py
+++ simpleWebTest/loadTestConclusion.py
@@ -10,4 +10,4 @@ for i in 1,2:
                 namespace['line%d' % i]= linecache.getline("output/loadTest_stats.csv",i)
                 result.append((namespace['line%d' % i].split(',')[j]))
 #print(*result, sep = ", ")
-print("It's the result of %s users(increaseing %s users/second).According to the output/oadTest_stats.csv the server is able to handle %s requests per second. In the final figure below,the median response time(green line)-is %s ms, and 95%% response time(yellow line) is %s ms then."%(totalUser,increment,result[4],result[3],result[5])) 
+print("It's the result of %s users(increaseing %s users/second).According to the output/loadTest_stats.csv the server is able to handle %s requests per second. In the final figure below,the median response time(green line)-is %s ms, and 95%% response time(yellow line) is %s ms then."%(totalUser,increment,result[4],result[3],result[5])) 
